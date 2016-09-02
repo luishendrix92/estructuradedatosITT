f6b4ca514c5a3753ff3180476c90a2d37219a216
Luis Felipe <luishendrix92@gmail.com>
2016-09-01 23:03:49 -0700

    Fibonacci

---
+++ fibonacci.cs
@@ -0,0 +1,30 @@
+using System;
+
+struct Fibonacci {
+  public static int FibR(int n) {
+    return (n <= 2)? 1 : FibR(n-1) + FibR(n-2);
+  } // Fin de algoritmo recursivo
+  
+  public static int FibI(int n) {
+    int fib = 1, ant = 1;
+    
+    for (int i = 0; i < n - 2; i++) {
+      fib = fib + ant;
+      ant = fib - ant;
+    }
+    
+    return fib;
+  } // Fin de algoritmo con iteración
+} // Fin de estructura de números fibonacci
+
+class Programa {
+  static void Main() {
+    Console.WriteLine("Qué N-ésimo término de la secuencia quieres?");
+    
+    int nFib = Int32.Parse(Console.ReadLine());
+    Console.WriteLine("N-ésimo términio recursivamente: {0}",
+      Fibonacci.FibR(nFib));
+    Console.WriteLine("N-ésimo término con iteración: {0}",
+      Fibonacci.FibI(nFib));
+  } // Fin de Método Main
+} // Fin de clase Programa
