bfd55fee6b20eddf17536044d3ef1cdd41bad842
Luis Felipe <luishendrix92@gmail.com>
2016-09-01 21:21:20 -0700

    Torre de Hanoi v1

--- conversiones.cs
+++ conversiones.cs
@@ -2,11 +2,7 @@ using System;
 using System.Collections.Generic;
 
 class Conversiones {
-  private const string ERROR_COMP_HEX = "El dígito no es compatible con el sistema hexadecimal";
-  
-  public static string ensamblarDigito(int n, int nuevaBase) {
-    if (n > 15 || n < 0) throw new Exception(ERROR_COMP_HEX);
-    
+  public static string EnsamblarDigito(int n, int nuevaBase) {    
     if (nuevaBase == 16) {
       return (n >= 10)? ((char)(n + 55)).ToString() : n.ToString();
     } else { // nuevaBases sin letras del alfabeto
@@ -14,13 +10,13 @@ class Conversiones {
     }
   }
   
-  public static void convertirDecimal(int n, int nuevaBase) {
+  public static void ConvertirDecimal(int n, int nuevaBase) {
     if (n >= nuevaBase) {
-      convertirDecimal(n / nuevaBase, nuevaBase);
+      ConvertirDecimal(n / nuevaBase, nuevaBase);
       
-      Console.Write("{0}", ensamblarDigito(n % nuevaBase, nuevaBase));
+      Console.Write("{0}", EnsamblarDigito(n % nuevaBase, nuevaBase));
     } else {
-      Console.Write("{0}", ensamblarDigito(n, nuevaBase));
+      Console.Write("{0}", EnsamblarDigito(n, nuevaBase));
     }
   } // Fin de conversión recursiva
 }
@@ -36,15 +32,15 @@ class Programa {
     
     // Imprimir conversiones en pantalla
     Console.Write("\nNúmero: {0}, en código binario:", numero);
-    Conversiones.convertirDecimal(numero, 2);
+    Conversiones.ConvertirDecimal(numero, 2);
     Console.Write("\n");
     
     Console.Write("\nNúmero: {0}, en sistema octal:", numero);
-    Conversiones.convertirDecimal(numero, 8);
+    Conversiones.ConvertirDecimal(numero, 8);
     Console.Write("\n");
     
     Console.Write("\nNúmero: {0}, en sistema hexadecimal:", numero);
-    Conversiones.convertirDecimal(numero, 16);
+    Conversiones.ConvertirDecimal(numero, 16);
     Console.Write("\n");
   }
 }
---
+++ hanoi.cs
@@ -0,0 +1,54 @@
+using System;
+
+class TorreDeHanoi {
+  public int discos;
+  public string origen, auxiliar, destino;
+
+  public int MinimoDeMovimientos {
+    get { return (int) Math.Pow(2, discos) - 1; }
+  } // Fórmula "M = 2^n - 1" donde "n" son los discos
+
+  public TorreDeHanoi
+  (int n = 3, string o = "1", string a = "2", string d = "3") {
+    discos   = n; origen   = o;
+    auxiliar = a; destino  = d;
+  } // Fin de constructor con valores default
+
+  /* PASO #2: Mover el disco que quedó en el
+  || origen hacia el destino definido o torre #3
+  \\==========================================*/
+  public static void MoverDiscos
+  (int discos, string origen, string aux, string destino) {
+    // Hay discos para mover? Moverlos, si no, termina recursión
+    if (discos > 0) {
+      /* PASO #1: Mover los discos de arriba (discos - 1)
+      || desde el inicio hacia la torre auxiliar.
+      \\===============================================*/
+      MoverDiscos(discos - 1, origen, destino, aux);
+
+      // Imprimir el movimiento que acabamos de hacer
+      Console.WriteLine("Mueve el disco {0} de la torre {1} a la torre {2}",
+        discos, origen, destino);
+
+      /* PASO #3: Mover los discos que quedaron en la
+      || torre auxiliar hacia el destino (discos - 1)
+      \============================================*/
+      MoverDiscos(discos - 1, aux, origen, destino);
+    } // Fin de comprobar si quedan discos por mover
+  } // Fin de proceso recursivo para resolver la torre
+
+  public void MostrarSolucion()
+  { TorreDeHanoi.MoverDiscos(discos, origen, auxiliar, destino); }
+} // Fin de la clase de Torre de Hanoi
+
+class Programa {
+  static void Main() {
+    Console.WriteLine("De cuántos discos quieres el juego?");
+    int numDiscos      = Int32.Parse(Console.ReadLine());
+    TorreDeHanoi torre = new TorreDeHanoi(numDiscos, "A", "B", "C");
+
+    Console.WriteLine("Este juego será resuelto en mínimo {0} movimientos",
+      torre.MinimoDeMovimientos);
+    torre.MostrarSolucion();
+  } // Fin de método principal
+} // Fin de clase programa
